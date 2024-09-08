f9677c253586a7e849ce095f93a66a1c15224f29
akoka <akoka@altimetrik.com>
2024-09-08 20:01:18 +0530

    commit2

--- tests/product_page_add_Item.spec.js
+++ tests/product_page_add_Item.spec.js
@@ -1,8 +1,8 @@
 import { test } from "@playwright/test"
 
-test("Product Page Add to Basket", async ({ page }) => {
-    await page.goto("localhost:2221")
-    await page.pause()
+test("Product Page Add to Basket",async ({page}) => {
+await page.goto("localhost:2221")
+await page.pause()
 })
 
 // const addTwoNumber = (a,b) => {
