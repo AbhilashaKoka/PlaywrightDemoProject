8b480d69b416f498d0f367994a17182aeab4bc68
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2024-09-08 19:55:37 +0530

    commit1

--- tests/product_page_add_Item.spec.js
+++ tests/product_page_add_Item.spec.js
@@ -1,10 +1,11 @@
 import { test } from "@playwright/test"
 
-test("Product Page Add to Basket", () => {
-
+test("Product Page Add to Basket", async ({ page }) => {
+    await page.goto("localhost:2221")
+    await page.pause()
 })
 
-const addTwoNumber = (a,b) => {
-    console.log("Adding up to two number:")
-    return  a + b
-}
+// const addTwoNumber = (a,b) => {
+//     console.log("Adding up to two number:")
+//     return  a + b
+// }
